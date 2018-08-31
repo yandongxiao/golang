d381d82d2dd1f805fce14d1f3ed017f6cdb8ce3e
yandongxiao <dxyan06@gmail.com>
2018-08-31 11:06:22 +0800

    update

---
+++ lib/net/http/request/server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func main() {
+	go func() {
+		http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+			// for log
+			fmt.Println(req.RemoteAddr)
+
+			// Usually the URL field should be used instead.
+			// NOTE: It is an error to set this field in an HTTP client request.
+			fmt.Println(req.RequestURI)
+
+			// ParseForm populates r.Form and r.PostForm.
+			// For all requests, ParseForm parses the **raw query** from the URL and updates r.Form.
+			// For POST, PUT, and PATCH requests, it also parses the request body as
+			//	a form and puts the results into both r.PostForm and r.Form.
+			// 所以，对于r.Form而言，请求体的Form值的优先级更高
+			// For other HTTP methods, or when the Content-Type is not
+			//	application/x-www-form-urlencoded, the request Body is not read, and
+			//	r.PostForm is initialized to a non-nil, empty value.
+			req.ParseForm()
+
+			// Form contains the parsed form data, including both the URL field's query parameters and the POST or PUT form data.
+			// This field is only available after ParseForm is called.
+			// 直接从url中读取query可能存在不全的问题
+			fmt.Println(req.Form)
+
+			// PostForm contains the parsed form data from POST, PATCH, or PUT body parameters.
+			// This field is only available after ParseForm is called.
+			// 只包括body内的form，明确了每个key的来源
+			fmt.Println(req.PostForm)
+
+			fmt.Println(req.Host)
+
+			resp.Write([]byte("server: helloworld"))
+		})
+		http.ListenAndServe("localhost:8080", nil)
+	}()
+
+	time.Sleep(time.Millisecond * 100)
+	http.Post("http://localhost:8080/helloworld?name=jack", "application/x-www-form-urlencoded", strings.NewReader("age=10"))
+}
---
+++ lib/net/http/request/trailer.go
@@ -0,0 +1,52 @@
+// Trailer specifies additional headers that are sent after the request body.
+// 有些Header可能需要边读请求体，边计算得到。但是发送完请求体后，和发送Trailer之间是如何控制的?
+// 我们应该构建自己的Reader，作为参数传递给req.Body. 这样就可以控制Trailer的值了
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func main() {
+	var err error
+	go server()
+	time.Sleep(time.Millisecond * 100)
+	req, err := http.NewRequest("POST", "http://localhost:8080?name=jack&age=1", strings.NewReader("helloworld"))
+	if err != nil {
+		panic(err)
+	}
+
+	// For client requests Trailer must be **initialized** to a map containing the trailer keys to later send.
+	// The values may be nil or their final values.
+	// 虽然Trailer的值虽然还不清楚，但是必须一开始就指定好key
+	req.Trailer = http.Header{
+		"XXX": []string{"xx", "yy"},
+	}
+
+	// The ContentLength must be 0 or -1, to send a chunked request.
+	req.ContentLength = -1
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	io.Copy(ioutil.Discard, resp.Body)
+}
+
+func server() {
+	http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+		// For server requests the Trailer map initially contains only the trailer keys, with nil values.
+		fmt.Println(req.Trailer)
+		// After reading from Body returns EOF, Trailer can be read again and will contain non-nil values
+		io.Copy(ioutil.Discard, req.Body)
+		fmt.Println(req.Trailer)
+		resp.Write([]byte("server: helloworld"))
+	})
+	http.ListenAndServe("localhost:8080", nil)
+}
