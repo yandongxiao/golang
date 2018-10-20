053741bcc914b9e68bfe1160f55f94de642b5ce4
yandongxiao <dxyan06@gmail.com>
2018-10-20 16:30:01 +0800

    update

---
+++ lib/sync/atomic/value.go
@@ -0,0 +1,44 @@
+// Value的优势就是在于定时任务更新数据，其它协程共享数据
+// NOTE: All calls to Store for a given Value must use values of the same concrete type.
+//       Store of an inconsistent type panics, as does Store(nil).
+package main
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+func loadConfig() map[string]string {
+	return make(map[string]string)
+}
+
+func requests() chan int {
+	return make(chan int)
+}
+
+func main() {
+	var config atomic.Value // holds current server configuration
+	// Create initial config value and store into config.
+	config.Store(loadConfig())
+
+	go func() {
+		// Reload config every 10 seconds
+		// and update config value with the new version.
+		for {
+			time.Sleep(10 * time.Second)
+			config.Store(loadConfig())
+		}
+	}()
+
+	// Create worker goroutines that handle incoming requests
+	// using the latest config value.
+	for i := 0; i < 10; i++ {
+		go func() {
+			for r := range requests() {
+				c := config.Load()
+				// Handle request r using config c.
+				_, _ = r, c
+			}
+		}()
+	}
+}
