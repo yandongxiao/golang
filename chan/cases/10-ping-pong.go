f67e6779c08676676d6a33a2a5653c686500603c
yandongxiao <dxyan06@gmail.com>
2019-10-23 13:50:17 +0800

    update

---
+++ chan/cases/10-ping-pong.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+import "time"
+import "os"
+
+type Ball uint64
+
+func Play(playerName string, table chan Ball) {
+	var lastValue Ball = 1
+	for {
+		ball := <-table // get the ball
+		fmt.Println(playerName, ball)
+		ball += lastValue
+		if ball < lastValue { // overflow
+			os.Exit(0)
+		}
+		lastValue = ball
+		table <- ball // bat back the ball
+		time.Sleep(time.Second)
+	}
+}
+
+func main() {
+	table := make(chan Ball)
+	go func() {
+		table <- 1 // throw ball on table
+	}()
+	go Play("A:", table)
+	Play("B:", table)
+}
---
+++ chan/cases/9-counting-semaphore.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"time"
+)
+
+type Seat int
+type Bar chan Seat
+
+func (bar Bar) ServeCustomer(c int) {
+	log.Print("customer#", c, " enters the bar")
+	seat := <-bar // need a seat to drink
+	log.Print("++ customer#", c, " drinks at seat#", seat)
+	time.Sleep(time.Second * time.Duration(2+rand.Intn(6)))
+	log.Print("-- customer#", c, " frees seat#", seat)
+	bar <- seat // free seat and leave the bar
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	// the bar has 10 seats.
+	bar24x7 := make(Bar, 10)
+	// Place seats in an bar.
+	for seatId := 0; seatId < cap(bar24x7); seatId++ {
+		// None of the sends will block.
+		bar24x7 <- Seat(seatId)
+	}
+
+	// 相当于请求在不断的到来，每个请求对应一个协程。通过Counting Semaphore机制，限制处理请求的最大并发数。
+	for customerId := 0; ; customerId++ {
+		time.Sleep(time.Second)
+		go bar24x7.ServeCustomer(customerId)
+	}
+
+	// sleeping != blocking
+	for {
+		time.Sleep(time.Second)
+	}
+}
