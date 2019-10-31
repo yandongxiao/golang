18f2ea39a56fcd02e979fc5fd3c0441abb10475b
yandongxiao <dxyan06@gmail.com>
2019-10-31 11:35:08 +0800

    update

--- interface/pointer_test.go
+++ interface/pointer_test.go
@@ -47,12 +47,7 @@ func ExampleSetter() {
 	}, name string) {
 		setter.set(name)
 	}
-	// interface type的值有两个：具体类型和具体对象.
-	// setter = p ：具体类型为Person, 具体对象是p的一个副本.
-	// set操作将会作用域p的这个副本，1. 很可能与开发的愿意不符，编译出错
-	// 2. 修改这个副本对象是没有什么意义的
-	// setter(p, "set")
-	setter(&p, "set")
+	setter(&p, "set") //setter(p, "set") 会导致编译出错
 	fmt.Println(p)
 
 	// Output:
