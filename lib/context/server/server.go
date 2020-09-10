1925a2f912e75807c744e84dcd4d144cf8dbc2dc
yandongxiao <dxyan06@gmail.com>
2020-09-11 01:41:36 +0800

    update

--- lib/context/server/server.go
+++ lib/context/server/server.go
@@ -1,25 +1,29 @@
-package server
+package main
 
 import (
 	"fmt"
-	"log"
+	stdlog "log"
 	"net/http"
 	"time"
+
+	"github.com/yandongxiao/golang-learning/lib/context/log"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("handler started")
-		defer log.Println("handler ended")
-		ctx := r.Context()
-		select {
-		case <-time.After(5 * time.Second):
-			_, _ = fmt.Fprintln(w, "hello")
-		case <-ctx.Done():
-			err := ctx.Err()
-			log.Println(err)
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-	})
-	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
+	http.HandleFunc("/", log.Decorate(handler))
+	stdlog.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	log.Println(ctx, "handler started")
+	defer log.Println(ctx, "handler ended")
+	select {
+	case <-time.After(5 * time.Second):
+		_, _ = fmt.Fprintln(w, "hello")
+	case <-ctx.Done():
+		err := ctx.Err()
+		log.Println(ctx, err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
 }
